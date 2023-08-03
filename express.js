5e27b7a07d30a4c051d804bd7643a5a10f1b6d55
AnderGI <agibarguen@gmail.com>
2023-08-03 09:26:47 +0200

    Ejercicio 3

--- express.js
+++ express.js
@@ -5,15 +5,21 @@ const fs = require("node:fs");
 
 function startServer() {
   const server = express();
-
+  server.disable("x-powered-by");
+  //Solo metodos post y application/json el json queda dentro del req.body
   server.use(express.json());
+
+  //Publica al cliente los recursos dentro de assets (si hubiese más, publicaria todos?)
   server.use(express.static("assets"));
-  //si ruta soporta un metodo bien, si no 405
-  //Todos son GET si no se dice lo contrario
+
+  //MIRO POR RUTAS Y DENTRO DE CADA UNA POR LOS METODOS QUE SOPORTA
+  //SI RUTA NO SOPORTA METODO -> 405
+  //SI RUTA NO EXISTE 404
   server.all("/", (req, res) => {
     if (req.method === "GET") {
-      return res.send("<h1>¡Hola mundo!</h1>");
+      return res.send("<h1>¡Hola mundo!</h1>"); //por defecto status 200 y Content-Type : text/html con el charset utf8
     } else {
+      //ruta solo soportaba el metodo GET los demas 405
       return res.status(405).send("<h1>405</h1>");
     }
   });
@@ -22,6 +28,14 @@ function startServer() {
     if (req.method === "GET") {
       return res.status(404).send("<h1>404</h1>");
     } else {
+      //ruta solo soportaba el metodo GET los demas 405
+      return res.status(405).send("<h1>405</h1>");
+    }
+  });
+
+  //Manejar posibles acciones http en la ruta /logo.webp
+  server.all("/logo.webp", (req, res) => {
+    if (req.method !== "GET") {
       return res.status(405).send("<h1>405</h1>");
     }
   });
@@ -30,15 +44,17 @@ function startServer() {
   //con el url /contacto bien sino 405 porque no soporta esa ruta la accion POST
   server.all("/contacto", (req, res) => {
     if (req.method === "POST") {
+      //el middleware express.json() añade al req.body el json en metodos post de content-type application/json
       return res.status(201).json(req.body);
     } else {
+      //ruta solo soportaba el metodo POST los demas 405
+      return res.status(405).send("<h1>405</h1>");
     }
-    return res.status(405).send("<h1>405</h1>");
   });
 
   //si no existe 404 ninguna de las url anteriores
   server.use((req, res) => {
-    req.status(404).send("<h1>404</h1>");
+    return res.status(404).send("<h1>404</h1>");
   });
 
   return server.listen(PORT, () => {
@@ -46,6 +62,9 @@ function startServer() {
   });
 }
 
+startServer();
+/*
 module.exports = {
   startServer,
 };
+*/
